268c541ed840bf74e4e53234d93c3200a3a8bc0c
William Nelson <bnelson@arabidopsis.org>
2011-09-19 07:58:40 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '1420bb7489eb361f44241fe356b72f2e78967b69bf441672d1b7910c77419deab094fa5603f3513e18127f303d8110ef05229e516d2d102e6d8f747044f65256'
