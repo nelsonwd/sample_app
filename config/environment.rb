619f729ebbb093bc02e970b25fa7cdc82d26d00c
William Nelson <bnelson@arabidopsis.org>
2011-11-09 09:36:07 -0800

    Done with user edit/update, index and destroy actions

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+module ConstantTest
+Constant = "I'm a constant"
+end
